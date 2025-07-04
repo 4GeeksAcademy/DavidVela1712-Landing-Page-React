3f0ff8840da53e317e442f4abfe0b5b4f5606986
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-07-04 18:26:35 +0000

    Cambio h1 a p con display

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 function Welcome() {
   return (
     <div>
-          <h1>Welcome</h1>
+          <p className='display-1'>Welcome</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
           <Button variant="primary">Call to action</Button>
     </div>
